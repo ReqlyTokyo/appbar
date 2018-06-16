fb0395a8fce66430176c748350c11dfb6c1edcb2
MitsukiUsui <mitsuo.com.mu@gmail.com>
2018-06-16 17:32:20 +0900

    ReqlyAppBar as class

--- myapp/src/ReqlyAppBar.js
+++ myapp/src/ReqlyAppBar.js
@@ -3,16 +3,18 @@ import SearchBar from './SearchBar'
 import DefaultBar from './DefaultBar'
 
 
-function ReqlyAppBar(props) {
-  return (
-    <div>
-      <DefaultBar />
-      <SearchBar
-        onChange={() => console.log('onChange')}
-        onRequestSearch={() => console.log('onRequestSearch')}
-      />
-    </div>
-  );
+class ReqlyAppBar extends React.Component{
+  render () {
+    return (
+      <div>
+        <DefaultBar />
+        <SearchBar
+          onChange={() => console.log('onChange')}
+          onRequestSearch={() => console.log('onRequestSearch')}
+        />
+      </div>
+    );
+  }
 }
 
 export default ReqlyAppBar;
