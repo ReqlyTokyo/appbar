b460431afea1d4f61dadfe22d9654e60d49b32ea
MitsukiUsui <mitsuo.com.mu@gmail.com>
2018-06-16 16:23:44 +0900

    imported SeachBar.js

--- myapp/src/ReqlyAppBar.js
+++ myapp/src/ReqlyAppBar.js
@@ -1,33 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import SearchBar from './SearchBar'
 
-const styles = {
-  root: {
-    flexGrow: 1,
-  },
-};
 
-function SimpleAppBar(props) {
+function ReqlyAppBar(props) {
   const { classes } = props;
   return (
-    <div className={classes.root}>
-      <AppBar position="static" color="default">
-        <Toolbar>
-          <Typography variant="title" color="inherit">
-            Title
-          </Typography>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <SearchBar
+      onChange={() => console.log('onChange')}
+      onRequestSearch={() => console.log('onRequestSearch')}
+    />
   );
 }
 
-SimpleAppBar.propTypes = {
+ReqlyAppBar.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
-export default withStyles(styles)(SimpleAppBar);
+export default ReqlyAppBar;
